c39348d0c4942a19a47d27828fefc9feb5125a61
Cole Finlayson <cole.fin8@gmail.com>
2020-10-28 18:57:14 -0600

    finished review

---
+++ src/components/Header.js
@@ -0,0 +1,22 @@
+import React from "react";
+import logo from "../navbar-logo.svg";
+
+export default function Header() {
+  return (
+    <header className="welcome-header">
+      <div className="logo-menu-box">
+        <a href="http://localhost:3000">
+          <img className="logo-svg" src={logo} />
+        </a>
+        <button className="menu-button-mobile">MENU &#9776;</button>
+      </div>
+      <ul className="nav-link-box">
+        <li className="nav-link">SERVICES</li>
+        <li className="nav-link">PORTFOLIO</li>
+        <li className="nav-link">ABOUT</li>
+        <li className="nav-link">TEAM</li>
+        <li className="nav-link">CONTACT</li>
+      </ul>
+    </header>
+  );
+}
